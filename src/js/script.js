55851a6714ff5f42fc4cc37d873ee75b91cd1877
DeyvesDion <deyvesdion@gmail.com>
2021-12-08 16:58:13 +0100

    sticky menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,20 @@
+// Declaration des variables
 const getOverlay = document.querySelector(".overlay");
 const closeDesktop = document.querySelector(".closeBtnDesktop");
 const closeMobile = document.querySelector(".closeBtnMobile");
-
+// fonction menu sticky
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+// Fonction toogle menu
 function toggleMenu() {
     const menuToggle = document.querySelector(".toggle");
     const menu = document.querySelector(".menu");
     menuToggle.classList.toggle("active")
     menu.classList.toggle("active")
 };
-
+// fonction pour definir le delais de overlay
 window.addEventListener("load", () => { setTimeout(() => { getOverlay.classList.add("show"); }, 2000) });
 closeDesktop.addEventListener("click", () => { getOverlay.classList.remove("show"); });
 closeMobile.addEventListener("click", () => { getOverlay.classList.remove("show"); })
